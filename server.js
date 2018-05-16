1ea882efdb931a763b4d0e3410b443cd3a04c6bc
Mridul Garg <mridul.garg@nagarro.com>
2018-05-16 14:36:11 +0530

    Index path bug1

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/dist/lms'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/lms/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 7777,()=>{
